6b525cc8fbe2ec53dca65d7897c9a98f96a76892
ajay poddar <ajay.poddar15fea@gmail.com>
2023-08-17 18:42:53 +0530

    final commit

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    img: "/src/assets/visigo.png",
+    title: "Travel WebApp",
+    desc: "React App",
+    link: "https://visigo.netlify.app/",
+    git: "https://github.com/ajaypoddar78890/visigo",
+  },
+  {
+    id: 2,
+    img: "/src/assets/imdbrating.png",
+    title: "IMDB Rating ",
+    desc: " React App",
+    link: "https://imdb-ratings.netlify.app/",
+    git: "",
+  },
+  {
+    id: 3,
+    img: "/src/assets/deals4wheels.png",
+    title: "MoterDealship",
+    desc: "UserID:seller Password:seller  FullStackAPP",
+    link: "https://deals4wheelss.netlify.app/",
+    git: "https://github.com/ajaypoddar78890/Deels4Wheelbackend",
+  },
+  {
+    id: 3,
+    img: "/src/assets/marcap.png",
+    title: "Ecommers WebApp",
+    desc: "UserID:testing@gmail.com Password:testing     FullStackAPP",
+    link: " https://marcap.netlify.app/",
+    git: " https://github.com/ajaypoddar78890/marcapecommers",
+  },
+];
