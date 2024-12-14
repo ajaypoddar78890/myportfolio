fe3f71fbfa48591ecabf4c1d88ad38db56346dc1
ajaypoddar78890 <ajay.poddar15fea@gmail.com>
2024-12-14 17:13:55 +0530

    fixing the bug

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -19,8 +19,8 @@ const Work = () => {
         </p>
       </div>
       {/* card */}
-      <div className="card-wrapper mx-auto w-[90%] sm:w-fit mt-5">
-        <div className="card-box grid grid-cols-3 space-y-5 space-x-5 w-full md:grid-cols-2 sm:gap-8 sm:grid-cols-1 sm:space-y-0 ">
+      <div className="card-wrapper mx-auto w-full   mt-5">
+        <div className="card-box grid grid-cols-2 space-y-2   w-full md:grid-cols-2 sm:gap-8 sm:grid-cols-1 sm:space-y-0 ">
           <WorkCard />
         </div>
       </div>
